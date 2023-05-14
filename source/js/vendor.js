e24d34dbc8a0346bd4f8431d02cf85c71d54c5b3
CirillSB <cirill2020@yandex.ru>
2023-05-14 21:10:01 +0700

    Собрал карточки

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,25 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
